f53177640c1a7d1715c1bbf6b0c0a36a65986621
Ezequiel <stuartsezequiel@gmail.com>
2019-12-14 14:32:52 -0300

    Carga de noticias, controladores y db

---
+++ app/Noticia.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Noticia extends Model
+{
+    public $table = "noticias";
+    //public $primarykey = "id";
+    //public $timestamps = "";
+    public $guarded = ['title', 'subtitle', 'date', 'img_noticia'];
+}
