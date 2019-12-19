a05207b42693ac548214af365e6a115574338b28
Ezequiel <stuartsezequiel@gmail.com>
2019-12-19 17:45:13 -0300

    arreglos para el pop up

--- public/js/popup-init.js
+++ public/js/popup-init.js
@@ -2,71 +2,68 @@
 /* las paginas que posean un contenedor "#popup" */
 
 $(function() {
-	$.ajax({
-		url: 'inc/modal-popup.html',
-		method: 'get',
-		data: null,
-		datatype: 'html',
-		async: false,
-		cache: false,
-		beforeSend: function(response) {
-			$('#popup').html('');
-		},
-		error: function(response, status, statustext) {
-			console.log({'response': response, 'status': status, 'statustext': statustext});
-		},
-		success: function(response) {
-			try {
-				$('#popup').html(response);
+    $.ajax({
+        url: "partials/pop-precalificate",
+        method: "get",
+        data: null,
+        datatype: "html",
+        async: false,
+        cache: false,
+        beforeSend: function(response) {
+            $("#popup").html("");
+        },
+        error: function(response, status, statustext) {
+            console.log({
+                response: response,
+                status: status,
+                statustext: statustext
+            });
+        },
+        success: function(response) {
+            try {
+                $("#popup").html(response);
 
+                // envío de formulario
+                // ------------------------------------------------------------
+                $("#precalif_form").on("submit", function(e) {
+                    e.preventDefault();
 
-				// envío de formulario
-				// ------------------------------------------------------------
-				$('#precalif_form').on('submit', function (e)	{
+                    $("#precalif_mensaje").html(
+                        "Enviando formulario, por favor espere..."
+                    );
 
-					e.preventDefault();
+                    $("#envio_precalif").attr("disabled", true);
 
-					$('#precalif_mensaje').html('Enviando formulario, por favor espere...');
+                    $.ajax({
+                        type: "post",
+                        url: "precalif.aspx",
+                        data: new FormData(this), // $('#precalif_form').serialize(),
+                        dataType: "html",
+                        cache: false,
+                        processData: false,
+                        contentType: false,
 
-					$('#envio_precalif').attr('disabled', true);
-					
-					$.ajax({
+                        success: function(data) {
+                            $("#precalif_mensaje").html(data);
+                            $("#envio_precalif").attr("disabled", false);
+                        },
 
-						type: 'post',
-						url: 'precalif.aspx',
-						data: new FormData(this), // $('#precalif_form').serialize(),
-						dataType: "html",
-						cache: false,
-						processData: false,
-						contentType: false,
-	
-						success: function (data) {
-							$('#precalif_mensaje').html(data);
-							$('#envio_precalif').attr('disabled', false);
-						},
-
-						error: function (data) {
-							$('#precalif_mensaje').html('Error al enviar formulario.');
-							$('#envio_precalif').attr('disabled', false);
-						}
-
-					});
-
-				});
-				// ------------------------------------------------------------
-				// fin de envío de formulario
-
-
-			} catch(e) {
-				console.log(response);
-				console.log(e);
-			}
-		}
-	});
+                        error: function(data) {
+                            $("#precalif_mensaje").html(
+                                "Error al enviar formulario."
+                            );
+                            $("#envio_precalif").attr("disabled", false);
+                        }
+                    });
+                });
+                // ------------------------------------------------------------
+                // fin de envío de formulario
+            } catch (e) {
+                console.log(response);
+                console.log(e);
+            }
+        }
+    });
 });
 
-
-$(function () {
-
-
-});
+$(function() {});
