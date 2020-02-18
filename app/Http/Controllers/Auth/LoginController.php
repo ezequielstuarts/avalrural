66ee4df6276430b309b221866856669c1fa8d9f0
Ezequiel <54673625+ezequielstuarts@users.noreply.github.com>
2020-02-17 22:36:45 -0300

    se agrego la columna content nulleable

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -11,7 +11,9 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+
     use AuthenticatesUsers;
 
-    protected $redirectTo = '/';
+    protected $redirectTo = 'admin';
 }
--- database/migrations/2020_01_16_212549_create_table_noticias.php
+++ database/migrations/2020_01_16_212549_create_table_noticias.php
@@ -19,7 +19,7 @@ class CreateTableNoticias extends Migration
             $table->datetime('date');
             $table->char('title', 255);
             $table->char('subtitle', 255);
-            $table->text('content');
+            $table->text('content')->nullable();
             $table->string('modified_by')->nullable();
             $table->char('img_preview', 255);
             $table->char('img_noticia', 255);
